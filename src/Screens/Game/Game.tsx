aec18a309e0fed7f4d92eff24824beb7ca084f4f
Chathu Abeywickrama <chathu.abey93@gmail.com>
2025-03-07 11:41:04 +0400

    more ingame navigation

---
+++ src/Screens/Game/Game.tsx
@@ -0,0 +1,43 @@
+import React, { useEffect } from 'react';
+import { View } from 'react-native';
+import { useSelector, useDispatch } from 'react-redux';
+import { getUserDetails } from '../../Utils/redux/slices/user/selector';
+import AuthService from '../../Utils/api/AuthService';
+import { startGameSession } from '../../Utils/redux/slices/game/slice';
+
+// Components
+import { TicTacToeBoard } from '../../Components';
+
+// Styles
+import { styles } from './styles';
+
+function GameScreen() {
+  const dispatch = useDispatch();
+  const userDetails = useSelector(getUserDetails);
+
+  const startNewGameSession = async () => {
+    try {
+      const startSession = await AuthService.createGameSession();
+      if (startSession) {
+        dispatch(startGameSession(startSession));
+      }
+    } catch (error) {
+      console.error('Register error', error);
+    }
+  };
+
+  useEffect(() => {
+    if (userDetails) {
+      console.log(userDetails, 'ud');
+      startNewGameSession();
+    }
+  }, [userDetails]);
+
+  return (
+    <View style={styles.container}>
+      <TicTacToeBoard />
+    </View>
+  );
+}
+
+export default GameScreen;
